f8355fe205694cf1c6bf434cc2ea311363e4dd8e
Arazor <arazor@laposte.net>
2015-03-15 14:51:27 +0100

    Revert "Signed-off-by: Arazor <arazor@laposte.net>"
    This reverts commit d48490193a7bf4aeb647645c190b6a9ce9fca843.

--- Terminal.py
+++ Terminal.py
@@ -5,8 +5,8 @@ import time
 import colorama
 from ctypes import windll, wintypes, byref
 
-STDIN = -10 #Assigne le flux d'entree a STDIN
-STDOUT = -11 #Assigne le flus de sortie a STDOUT
+STDIN = -10 #Assigne le flux d'entree a  STDIN
+STDOUT = -11 #Assigne le flus de sortie a  STDOUT
 
 
 colorama.init() #Initialise colorama que permet de manipuler les codes d'echape AINSI
@@ -16,7 +16,7 @@ sortie = windll.kernel32.GetStdHandle(STDOUT) #Assigne le flux de sortie a  sort
 rectangle = wintypes.SMALL_RECT(0, 0, 63, 31) #Cree un objet wintypes _SALL_RECT demande par SetConsoleWindowInfo de 64 et 32 lignes (Gauche, Haut, Droite, Bas).
 
 windll.kernel32.SetConsoleTitleA("Rogue Exporer") #Assigne le titre "Rogue Exporer" a  la console.
-sys.stdout.write("\033[31m") #\033 correspond au code d'echape AINSI qui permet de manipuler des textes dans la console de commande 31 correspond a la couleur rouge, le reste est de la syntaxe.
+sys.stdout.write("\033[31m") #\033 correspond au code d'echape AINSI qui permet de manipuler des textes dans la console de commande 31 correspond a  la couleur rouge, le reste est de la syntaxe.
 
 #Bloc de texte.
 print("OOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO")
